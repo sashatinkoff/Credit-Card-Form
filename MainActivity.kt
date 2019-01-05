4ae6375f54f5d2f5b254babec792b2501a53e780
Aleksandr Tamakov <sashatinkoff@gmail.com>
2019-01-05 16:54:55 +0500

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,34 @@
+package com.isidroid.a18
+
+import android.os.Bundle
+import com.isidroid.a18.databinding.ActivityMainBinding
+import com.isidroid.creditcard.CreditCardForm
+import com.isidroid.utils.BindActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
+
+class MainActivity : BindActivity<ActivityMainBinding>() {
+    override val resource = R.layout.activity_main
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val creditCards = CreditCardForm(creditCardInput)
+                .defaults()
+                .cvsInput(cvsInput)
+                .expiresInput(expiresInput)
+                .focusChains(creditCardInput, expiresInput, cvsInput, btnRead)
+                .onCardType { type -> textview.text = "$type" }
+                .onFormComplete { btnCar.isEnabled = it }
+                .create()
+
+        btnRead.setOnClickListener {
+            Timber.i("card='${creditCards.cardNumberRaw()}'\n" +
+                    "formatted=${creditCards.cardNumberFormatted()}\n" +
+                    "expires=${creditCards.expires()}\n" +
+                    "expiresDate=${creditCards.expiresDate()}\n" +
+                    "cvs=${creditCards.cvs()}\n" +
+                    "isComplete=${creditCards.isComplete()}")
+        }
+    }
+}
